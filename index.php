9ab6d3de1e27a05256425c1ca07c27737291efca
Amir Hossein Bagheri <avazgram.com@gmail.com>
2025-01-22 14:53:18 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: پرداخت آنلاین ووکامرس
-Plugin URI: /
+Plugin URI: https://github.com/ahbagheri79/WC-payments.git
 Version: 1.0.0
 Description: پلاگین درگاه پرداخت ووکامرس، پشتیبانی از: ایران کیش  -  درگاه های بیشتری به زودی اضافه میگردد
 Author: امیر حسین باقری
